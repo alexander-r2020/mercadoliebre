b2a7f9828bc1f8096ee5f554781dd42ff3fea219
Alexander Ruben Mamani <labm2014@gmail.com>
2021-09-01 22:51:26 -0300

    agregando rutas y corrigiendo las tarjetas

--- app.js
+++ app.js
@@ -9,5 +9,9 @@ app.use(express.static('public'));
 /*******Estableciendo mi Pagina HOME como inicio*** */
 app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'views','home.html')))
 
+/**********Estableciendo pagina Register  y login*********/
+app.get('/register',(req,res)=>res.sendFile(path.join(__dirname,'views','register.html')))
+app.get('/login',(req,res)=>res.sendFile(path.join(__dirname,'views','login.html')))
+
 /********Iniciando Servidor al final de todo****** */
 app.listen(port,()=> console.log("estoy en el puerto " + port))
